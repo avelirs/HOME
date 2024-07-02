d929bdc6d16aa62c8d131346645880e2dcbc52fc
avelirs <sibilevanton6@gmail.com>
2024-07-02 15:10:24 +0300

    init:: change a files

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <div className='flex'>
+      <div></div>
+      <nav className='space-x-4'>
+        <a href='#' className='text-white'>
+          Homes
+        </a>
+        <a href='#' className='text-white'>
+          Cities
+        </a>
+        <a href='#' className='text-white'>
+          Policy
+        </a>
+      </nav>
+    </div>
+  );
+}
